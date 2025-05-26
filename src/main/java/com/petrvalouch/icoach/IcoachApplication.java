2a0071114986e65dfc22ca13b363063bb897ee7b
petrdragonman <petrdragonman@me.com>
2025-05-26 14:40:08 +1000

    first commit

---
+++ src/main/java/com/petrvalouch/icoach/IcoachApplication.java
@@ -0,0 +1,13 @@
+package com.petrvalouch.icoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcoachApplication.class, args);
+	}
+
+}
