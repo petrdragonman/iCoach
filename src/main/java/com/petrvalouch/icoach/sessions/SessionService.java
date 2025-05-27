92cc27c502cd7e7cc5cbbeaae5b1572d25ddac02
petrdragonman <petrdragonman@me.com>
2025-05-27 14:56:35 +1000

    create session

--- src/main/java/com/petrvalouch/icoach/sessions/SessionService.java
+++ src/main/java/com/petrvalouch/icoach/sessions/SessionService.java
@@ -1,7 +1,9 @@
 package com.petrvalouch.icoach.sessions;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class SessionService {
     
     private SessionRepository repo;
@@ -12,6 +14,9 @@ public class SessionService {
         this.mapper = mapper;
     }
 
-    
+    public Session createSession(CreateSessionDTO data) {
+        Session newSession = mapper.map(data, Session.class);
+        return this.repo.save(newSession);
+    }
     
 }
