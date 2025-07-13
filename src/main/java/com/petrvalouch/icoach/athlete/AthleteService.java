8269c7389ee2f32409e7788e944b5369e0dc606c
petrdragonman <petrdragonman@me.com>
2025-07-13 16:48:10 +1000

    implements feature athlete at backend - CRUD

--- src/main/java/com/petrvalouch/icoach/athlete/AthleteService.java
+++ src/main/java/com/petrvalouch/icoach/athlete/AthleteService.java
@@ -1,6 +1,7 @@
 package com.petrvalouch.icoach.athlete;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,29 @@ public class AthleteService {
     public Athlete createAthlete(CreateAthleteDTO data) {
         Athlete newAthlete = mapper.map(data, Athlete.class);
         return this.repo.save(newAthlete);
+    }
+
+    public Optional<Athlete> getById(Long id) {
+        return this.repo.findById(id);
+    }
+
+    public boolean deleteById(Long id) {
+        Optional<Athlete> result = this.getById(id);
+        if (result.isEmpty()) {
+            return false;
+        }
+        this.repo.delete(result.get());
+        return true;
+    }
+
+    public Optional<Athlete> updateById(Long id, UpdateAthleteDTO data) {
+        Optional<Athlete> result = this.getById(id);
+        if (result.isEmpty()) {
+            return result;
+        }
+        Athlete foundAthlete = result.get();
+        mapper.map(data, foundAthlete);
+        this.repo.save(foundAthlete);
+        return Optional.of(foundAthlete);
     }
 }
